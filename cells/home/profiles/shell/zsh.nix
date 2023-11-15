a6203c4451d8ad12569ed98ecd54d1f72933b96f
Lorenzo Giuliani <lgiuliani@malwarebytes.com>
2023-11-15 13:22:23 +0100

    cleanup zsh

--- cells/home/profiles/shell/zsh.nix
+++ cells/home/profiles/shell/zsh.nix
@@ -39,7 +39,7 @@ in
 
     initExtra = ''
       if [ "$TMUX" = "" ]; then
-          tmux -u attach || tmux -u
+          tmux -u -2 attach || tmux -2 -u
       fi
 
       export GPG_TTY=$(tty)
@@ -52,37 +52,12 @@ in
       eman()  { emacs -nw --eval "(switch-to-buffer (man \"$1\"))"; }
       ekill() { emacsclient --eval '(kill-emacs)'; }
 
-      ######################################### oh-my-zsh/lib/key-bindings.zsh #########################################
-      # Start typing + [Up-Arrow] - fuzzy find history forward
-      if [[ "''${terminfo[kcuu1]}" != "" ]]; then
-        autoload -U up-line-or-beginning-search
-        zle -N up-line-or-beginning-search
-        bindkey "''${terminfo[kcuu1]}" up-line-or-beginning-search
-      fi
-      # Start typing + [Down-Arrow] - fuzzy find history backward
-      if [[ "''${terminfo[kcud1]}" != "" ]]; then
-        autoload -U down-line-or-beginning-search
-        zle -N down-line-or-beginning-search
-        bindkey "''${terminfo[kcud1]}" down-line-or-beginning-search
-      fi
-      bindkey '^[[127;5u' backward-kill-word                  # [Ctrl-Backspace] - delete whole backward-wordf3
-      bindkey '^[[127;2u' backward-kill-word                  # [Shift-Backspace] - delete whole backward-word
-      bindkey '^[[127;4u' backward-kill-line                  # [Shift-Alt-Backspace] - delete line up to cursor
-      bindkey '^[[3;5~' kill-word                             # [Ctrl-Delete] - delete whole forward-word
-      bindkey '^[[3;2~' kill-word                             # [Shift-Delete] - delete whole forward-word
-      bindkey '^[[3;4~' kill-line                             # [Shift-Alt-Delete] - delete line from cursor
-      bindkey '^[[Z' reverse-menu-complete                    # [Shift-Tab] - move through the completion menu backwards
-      bindkey '^[[1;5C' forward-word                          # [Ctrl-RightArrow] - move forward one word
-      bindkey '^[[1;5D' backward-word                         # [Ctrl-LeftArrow] - move backward one word
-      ##################################################################################################################
-
       source ${nixpkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
       autoload -Uz add-zsh-hook
       _iay_prompt() {
-        # PROMPT="$(iay -z)"    # regular variant
-        PROMPT="$(iay -zm)" # miminal variant
-        unset RPROMPT
+        # PROMPT="$(${nixpkgs.iay}/bin/iay -zm)" # regular variant
+        PROMPT="$(${nixpkgs.iay}/bin/iay -zm)"   # miminal variant
       }
       add-zsh-hook precmd _iay_prompt
     '';
@@ -96,11 +71,9 @@ in
 
   home.packages = with nixpkgs; [
     zsh-vi-mode
-    zsh-history
     zsh-fzf-tab
     zsh-autopair
     zsh-fast-syntax-highlighting
-    fzf-zsh
     iay
   ];
 }
