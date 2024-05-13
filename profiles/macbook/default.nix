4bde3faf85568c2cc3e56e37689dd6e1306e86a4
Lorenzo Giuliani <lorenzo@quantfi.tech>
2024-05-13 10:56:20 +0200

    macbook: add shells to environment shells

--- profiles/macbook/default.nix
+++ profiles/macbook/default.nix
@@ -4,18 +4,6 @@
     extra-experimental-features = flakes nix-command
   '';
 
-  # https://github.com/LnL7/nix-darwin/issues/158#issuecomment-974598670
-  programs.zsh.enable = true;
-  programs.zsh.shellInit = ''export OLD_NIX_PATH="$NIX_PATH";'';
-  programs.zsh.interactiveShellInit = ''
-    if [ -n "$OLD_NIX_PATH" ]; then
-      if [ "$NIX_PATH" != "$OLD_NIX_PATH" ]; then
-        NIX_PATH="$OLD_NIX_PATH"
-      fi
-      unset OLD_NIX_PATH
-    fi
-  '';
-
   # Recreate /run/current-system symlink after boot
   services.activate-system.enable = true;
 
@@ -76,10 +64,26 @@
 
   fonts.fontDir.enable = true;
 
+  # https://github.com/LnL7/nix-darwin/issues/158#issuecomment-974598670
+  programs.zsh.enable = true;
+  programs.zsh.shellInit = ''export OLD_NIX_PATH="$NIX_PATH";'';
+  programs.zsh.interactiveShellInit = ''
+    if [ -n "$OLD_NIX_PATH" ]; then
+      if [ "$NIX_PATH" != "$OLD_NIX_PATH" ]; then
+        NIX_PATH="$OLD_NIX_PATH"
+      fi
+      unset OLD_NIX_PATH
+    fi
+  '';
+
   # https://github.com/nix-community/home-manager/issues/4026
   # fish shell setup
   programs.fish.enable = true;
-  environment.shells = [pkgs.fish];
+
+  environment.shells = [
+    pkgs.zsh pkgs.fish
+  ];
+
   users.users.lorenzo = {
     home = "/Users/lorenzo";
     shell = pkgs.fish;
