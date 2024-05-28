6fa3c42c9e1a27a4a43ce2328d6a3d8bdae07c60
Lorenzo Giuliani <lorenzo@quantfi.tech>
2024-05-28 10:42:34 +0200

    use vertico

--- home/doom.d/init.el
+++ home/doom.d/init.el
@@ -22,14 +22,16 @@
 
        :completion
        ;;company           ; the ultimate code completion backend
-       (corfu +orderless
-              +icons
-              +dabbrev
-              )  ; complete with cap(f), cape and a flying feather!
+       ;;(company +childframe)
+       ;;(corfu +orderless
+       ;;       +icons
+       ;;       +dabbrev
+       ;;       )  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       ;;vertico           ; the search engine of the future
+       (vertico +childframe +icons)
 
        :ui
        ;;deft              ; notational velocity for Emacs
