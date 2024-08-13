8aafaeb2e849d203a03daad666cad1810ead95ec
Lorenzo Giuliani <lorenzo@frenzart.com>
2024-08-13 08:54:22 +0200

    home/helix: config

--- home/helix.nix
+++ home/helix.nix
@@ -8,12 +8,23 @@
         cursor-shape.insert = "bar";
         line-number = "relative";
         lsp.display-messages = true;
+        lsp.display-inlay-hints = true;
       };
       keys.normal = {
         space.space = "file_picker";
         space.w = ":w";
         space.q = ":q";
         esc = [ "collapse_selection" "keep_primary_selection" ];
+        space.f = [
+          ":new"
+          ":insert-output lf -selection-path=/dev/stdout"
+          "split_selection_on_newline"
+          "goto_file"
+          "goto_last_modification"
+          "goto_last_modified_file"
+          ":buffer-close!"
+          ":redraw"
+        ];
       };
     };
   };
