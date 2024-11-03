a007c24af79828c9f4a03bed7233fce1e13c4744
Lorenzo Giuliani <lorenzo@frenzart.com>
2024-11-03 12:38:13 +0100

    gotosocial

--- profiles/frenz/_services.nix
+++ profiles/frenz/_services.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ pkgs, ... }: {
   environment.systemPackages = [ pkgs.caddy ];
 
   services.caddy = {
@@ -6,6 +6,26 @@
     email = "lorenzo@frenzart.com";
 
     virtualHosts = {
+      "www.giuliani.me" = {
+        extraConfig = ''
+          redir https://frenz.click
+        '';
+      };
+      "giuliani.me" = {
+        extraConfig = ''
+          redir https://frenz.click
+        '';
+      };
+      "www.frenzart.com" = {
+        extraConfig = ''
+          redir https://frenz.click
+        '';
+      };
+      "frenzart.com" = {
+        extraConfig = ''
+          redir https://frenz.click
+        '';
+      };
       "www.frenz.click" = {
         extraConfig = ''
           redir https://frenz.click
@@ -35,6 +55,14 @@
           }
         '';
       };
+      "social.marrani.lol" = {
+        extraConfig = ''
+          encode zstd gzip
+          reverse_proxy http://127.0.0.1:10006 {
+            flush_interval -1
+          }
+        '';
+      };
     };
   };
 
@@ -50,6 +78,28 @@
       Listen = "10.69.0.1";
       Allow = "10.69.0.0/24";
       Timeout = 60;
+    };
+  };
+
+  services.gotosocial = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      host = "social.marrani.lol";
+      account-domain = "marrani.lol";
+      application-name = "social.marrani.lol";
+      bind-address = "127.0.0.1";
+      db-address = "/var/lib/gotosocial/database.sqlite";
+      db-type = "sqlite";
+      port = 10006;
+      protocol = "https";
+      storage-local-base-path = "/var/lib/gotosocial/storage";
+      instance-language = ["it" "en-us" "en-gb" "ru"];
+      instance-inject-mastodon-version = true;
+      accounts-registration-open= true;
+      accounts-reason-required= true;
+      accounts-allow-custom-css= true;
+      letsencrypt-enabled = false;
     };
   };
 }
