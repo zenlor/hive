fac49f52ccc935f5038f5a5e1b6757f84d088362
Lorenzo Giuliani <lorenzo@quantfi.tech>
2025-03-04 13:07:28 +0100

    fix gitui inadequacies

--- home/users/quantfi.nix
+++ home/users/quantfi.nix
@@ -1,6 +1,6 @@
 { ... }: {
-  programs.git.extraConfig.user.signingkey =
-    "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOGLWaOeeyPf8Pegp4q/PWCDFgtXoJ5dm4B4Gpw4SjwD";
+  programs.git.extraConfig.user.signingkey = "~/.ssh/id_ed25519.pub";
+    # "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOGLWaOeeyPf8Pegp4q/PWCDFgtXoJ5dm4B4Gpw4SjwD";
   programs.git.userName = "Lorenzo Giuliani";
   programs.git.userEmail = "lorenzo@quantfi.tech";
 }
