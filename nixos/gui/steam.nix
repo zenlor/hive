2b4b72a1d4a24cedf93b0631819793175ef34f88
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-04-09 08:58:44 +0200

    n/g/steam: enable steam controller

--- nixos/gui/steam.nix
+++ nixos/gui/steam.nix
@@ -12,10 +12,17 @@
 
   programs.gamescope = {
     enable = true;
+    capSysNice = true;
   };
+
+  # Steam controller
+  hardware.steam-hardware.enable = true;
+  services.joycond.enable = true;
 
   environment.systemPackages = with pkgs; [
     wineWowPackages.waylandFull
+
+    mangohud
 
     winetricks
     protontricks
@@ -25,4 +32,8 @@
     gamemode
     gnomeExtensions.gamemode-shell-extension
   ];
+
+
+  # Star citizen needs more
+  boot.kernel.sysctl."vm.max_map_count" = 16777216;
 }
