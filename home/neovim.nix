7bf5d51d437d6e9f275d1e011801f246b2020da4
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-04-10 21:17:58 +0200

    neovim shall speak man

--- home/neovim.nix
+++ home/neovim.nix
@@ -20,6 +20,11 @@
     ];
   };
 
+  # neovim shall be the pager of my man
+  home.sessionVariables = {
+    MANPAGER = "nvim +Man!";
+  };
+
   # xdg.configFile."nvim/after".source = ./neovim.d/after;
   xdg.configFile."nvim/lua".source = ./neovim.d/lua;
   xdg.configFile."nvim/init.lua".source = ./neovim.d/init.lua;
