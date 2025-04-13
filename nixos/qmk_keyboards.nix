1db25329b7da7a57736d3ecd93139e48fb7e723e
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-04-13 20:49:33 +0200

    qmk/via:add custom ergodox ez to udev rules

--- nixos/qmk_keyboards.nix
+++ nixos/qmk_keyboards.nix
@@ -1,6 +1,15 @@
 { ... }:
 { pkgs, lib, ... }:
 {
+  hardware.keyboard.qmk.enable = true;
+
+  environment.systemPackages = with pkgs; [
+      qmk
+      qmk-udev-rules
+      qmk_hid
+      via
+      vial   
+  ];
   services.udev = {
     packages = with pkgs; [
       qmk
@@ -10,4 +19,9 @@
       vial   
     ];
   };
+
+  # Ergodox EZ
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3297", ATTRS{idProduct}=="4975", OWNER="1000", GROUP="100", MODE="0666"
+  '';
 }
