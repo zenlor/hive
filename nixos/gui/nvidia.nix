857a8afdecb9a3c020885f6a41f94fff752bc653
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-04-13 08:54:43 +0200

    nvidia: 570.x

--- nixos/gui/nvidia.nix
+++ nixos/gui/nvidia.nix
@@ -1,5 +1,5 @@
 { ... }:
-{ pkgs, config, ... }:{
+{ pkgs, config, lib, ... }: {
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
@@ -8,12 +8,21 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
+  hardware.nvidia = let
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.133.07";
+      sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
+      sha256_aarch64 = "sha256-yTovUno/1TkakemRlNpNB91U+V04ACTMwPEhDok7jI0=";
+      openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
+      settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
+      persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
+    };
+  in {
     open = true;
     modesetting.enable = true;
     powerManagement.enable = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = package; # config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # boot params
