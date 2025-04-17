99095d8a5c697987ce9608a87949f8de0b82bc41
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-04-17 09:40:03 +0200

    nvidia vs systemd

--- nixos/gui/nvidia.nix
+++ nixos/gui/nvidia.nix
@@ -8,23 +8,31 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = let
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.133.07";
-      sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
-      sha256_aarch64 = "sha256-yTovUno/1TkakemRlNpNB91U+V04ACTMwPEhDok7jI0=";
-      openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
-      settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
-      persistencedSha256 = "sha256-G1V7JtHQbfnSRfVjz/LE2fYTlh9okpCbE4dfX9oYSg8=";
-    };
-  in {
+  hardware.nvidia = {
     open = true;
     modesetting.enable = true;
     powerManagement.enable = true;
     nvidiaSettings = true;
-    package = package; # config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # boot params
   boot.kernelParams = ["nvidia.NVreg_PerserveVideoMemoryAllocations=1"];
+
+  # nvidia Modprobe settings
+  # 
+  # https://discourse.nixos.org/t/psa-for-those-with-hibernation-issues-on-nvidia/61834
+  # https://forums.developer.nvidia.com/t/black-screen-with-cursor-after-sleep/319473/5
+  # boot.extraModprobeConfig = ''
+  #   options nvidia_modeset vblank_sem_control=0
+  #   options nvidia NVreg_PreserveVideoMemoryAllocations=0
+  # '';
+
+  systemd.units.systemd-suspend = {
+    overrideStrategy = "asDropin";
+    text = ''
+      [Service]
+      Environment="SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"
+    '';
+  };
 }
