6d7b6b5ee649a927b79d2cf4e8815ff228e13a71
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-05-11 11:37:32 +0200

    fix suspension when using gnome

--- nixos/gui/gnome.nix
+++ nixos/gui/gnome.nix
@@ -66,4 +66,45 @@
 
   # nobody asked for NM to be online :/
   systemd.services.NetworkManager-wait-online.enable = false;
+
+  # HACK:
+  # send STOP on suspend to gnome-shell
+  # send CONT on resume to gnome-shell
+  #
+  # somehow still relevant: https://discourse.nixos.org/t/suspend-resume-cycling-on-system-resume/32322/12
+  systemd.services = {
+     gnome-suspend = {
+      description = "suspend gnome shell";
+      before = [
+        "systemd-suspend.service" 
+        "systemd-hibernate.service"
+        "nvidia-suspend.service"
+        "nvidia-hibernate.service"
+      ];
+      wantedBy = [
+        "systemd-suspend.service"
+        "systemd-hibernate.service"
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
+      };
+    };
+    gnome-resume = {
+      description = "resume gnome shell";
+      after = [
+        "systemd-suspend.service" 
+        "systemd-hibernate.service"
+        "nvidia-resume.service"
+      ];
+      wantedBy = [
+        "systemd-suspend.service"
+        "systemd-hibernate.service"
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
+      };
+    };
+  };
 }
