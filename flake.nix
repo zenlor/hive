0fa653fe678f5d5f8f06811034bbb095b8d88f3a
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-05-20 20:42:35 +0200

    update nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -266,17 +266,4 @@
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  nixConfig = {
-    extra-trusted-substituters = [
-      "https://cache.garnix.io"
-      "https://cachix.org/api/v1/cache/emacs"
-      "https://nix-community.cachix.org"
-      "https://numtide.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-    ];
-  };
 }
