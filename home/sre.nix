76be3bb7b0acb0fceeed6ab862cc05c3ce4680c3
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-06-05 10:26:40 +0200

    configure k9s

--- home/sre.nix
+++ home/sre.nix
@@ -2,11 +2,26 @@
 {
   home.packages = with pkgs; [
     awscli2 # the worst official aws cli
-    k9s # nicer kubectls
     kubetui
     pre-commit # overengineered frameworks for simple things :facepalm:
     saml2aws # samling on aws over and over
     terraform-docs # documentation for terraform things
     terraform-ls # lsp for terraformation
   ];
+
+  programs.k9s = {
+    enable = true;    
+    settings = {
+      k9s = {
+        skin = "default";
+        ui = {
+          enableMouse = true;
+          headless = true;
+          logoless = true;
+        };
+        skipLatestRevCheck = true;
+        
+      };
+    };
+  };
 }
