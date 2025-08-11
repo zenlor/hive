12fa4aaeed15a1d74cc53df349f9e899c6e301f3
Lorenzo Giuliani <lorenzo@quantfi.tech>
2025-08-11 17:39:21 +0200

    color fish-hydro

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -18,6 +18,14 @@
       # delete erroneous commands at shell exit
       set sponge_purge_only_on_exit true
 
+      # hydro settings
+      set hydro_multiline false
+      set hydro_fetch true
+      set hydro_color_pwd $fish_color_cwd
+      set hydro_color_error $fish_color_error
+      set hydro_color_prompt $fish_color_operator
+      set hydro_color_git $fish_color_redirection
+
       set --universal pure_shorten_prompt_current_directory_length 1
       set --universal pure_enable_single_line_prompt true
       fish_vi_key_bindings
