1d98a5333a527a41d7eb241b876b7d87e4bfc7e7
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-09-17 15:50:49 +0200

    remove useless programs

--- nix/homeModules/shell.nix
+++ nix/homeModules/shell.nix
@@ -18,15 +18,14 @@
       # delete erroneous commands at shell exit
       set sponge_purge_only_on_exit true
 
-      # hydro settings
-      set hydro_multiline false
-      set hydro_color_pwd $fish_color_cwd
-      set hydro_color_error $fish_color_error
-      set hydro_color_prompt $fish_color_operator
-      set hydro_color_git $fish_color_redirection
+      # hydro prompt settings
+      set --global hydro_multiline false
+      set --global hydro_color_pwd $fish_color_cwd
+      set --global hydro_color_error $fish_color_error
+      set --global hydro_color_prompt $fish_color_operator
+      set --global hydro_color_git $fish_color_redirection
+      set --global hydro_cmd_duration_threshold 10000
 
-      set --universal pure_shorten_prompt_current_directory_length 1
-      set --universal pure_enable_single_line_prompt true
       fish_vi_key_bindings
     '';
     plugins = [
@@ -42,44 +41,7 @@
         name = "prompt";
         src = pkgs.fishPlugins.hydro.src;
       }
-      {
-        name = "forgit";
-        src = pkgs.fishPlugins.forgit.src;
-      }
     ];
-  };
-
-  programs.bottom = {
-    enable = true;
-    settings = {
-      flags = { regex = true; };
-      color = {
-        # https://github.com/catppuccin/bottom/blob/main/themes/mocha.toml
-        table_header_color = "#f5e0dc";
-        all_cpu_color = "#f5e0dc";
-        avg_cpu_color = "#eba0ac";
-        cpu_core_colors =
-          [ "#f38ba8" "#fab387" "#f9e2af" "#a6e3a1" "#74c7ec" "#cba6f7" ];
-        ram_color = "#a6e3a1";
-        swap_color = "#fab387";
-        rx_color = "#a6e3a1";
-        tx_color = "#f38ba8";
-        widget_title_color = "#f2cdcd";
-        border_color = "#585b70";
-        highlighted_border_color = "#f5c2e7";
-        text_color = "#cdd6f4";
-        graph_color = "#a6adc8";
-        cursor_color = "#f5c2e7";
-        selected_text_color = "#11111b";
-        selected_bg_color = "#cba6f7";
-        high_battery_color = "#a6e3a1";
-        medium_battery_color = "#f9e2af";
-        low_battery_color = "#f38ba8";
-        gpu_core_colors =
-          [ "#74c7ec" "#cba6f7" "#f38ba8" "#fab387" "#f9e2af" "#a6e3a1" ];
-        arc_color = "#89dceb";
-      };
-    };
   };
 
   programs.fzf = {
