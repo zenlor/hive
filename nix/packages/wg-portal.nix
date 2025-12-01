08a42246da44cee0d7c4c3260ea58729f843047e
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-12-01 19:12:25 +0100

    wg-portal pkg

--- nix/packages/wg-portal.nix
+++ nix/packages/wg-portal.nix
@@ -1,22 +1,65 @@
-{ lib
-, buildGoModule
-, fetchFromGithub
-,
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  buildNpmPackage,
 }:
-buildGoModule rec {
-  pname = "wg-portal";
-  version = "";
+let
+  version = "2.1.1";
 
-  src = fetchFromGithub {
+  sha256 = "sha256-3+/VIdlOOSUooRg5pceXdMqZQe7rG1wBEINJOqkKBP4="; # sources
+  npmDepsHash = "sha256-QpZUmr77tmLW6eGqL3emvoFDlrJiCebdpV8XN/a1u0o="; # npm/frontend
+  vendorHash = "sha256-EpQoXojJJ4AyQnOUeU7wb6RoTansn/aBQ3Qkeqhjxbg="; # server vendor
+
+  src = fetchFromGitHub {
     owner = "h44z";
     repo = "wg-portal";
-    rev = "master"; # FIXME
-    sha512 = "";
+    rev = "v${version}";
+    inherit sha256;
   };
 
+  frontend = buildNpmPackage {
+    pname = "wg-portal-frontend";
+
+    inherit version;
+    inherit npmDepsHash;
+
+    src = "${src}/frontend";
+    npmBuildScript = "build";
+    makeCacheWritable = true;
+    # output folder
+    DIST_OUT_DIR = "./dist";
+
+    preBuild = ''
+      # NOTE: sass-embedded is broken in NixOS. It fails at executing node_modules/sass-embedded-linux-x64/dart-sass/src/dart
+      # embedding dynamically linked binaries is always bad for the environment and my pressure
+      rm -r node_modules/sass-embedded*
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      ls -la dist/
+      mkdir -p $out
+      cp -rv dist/* $out/
+      runHook postInstall
+    '';
+  };
+in
+buildGoModule rec {
+  pname = "wg-portal";
+  inherit version;
+  inherit src;
+
   ldflags = [
-    "-s -w -X github.com/h44z/wg-portal/version=${version}"
+    "-s -w -X github.com/h44z/wg-portal/internal.Version=v${version}"
   ];
+
+  inherit vendorHash;
+
+  preBuild = ''
+    mkdir -p ./internal/app/api/core/frontend-dist/
+    cp -rv ${frontend}/* ./internal/app/api/core/frontend-dist/
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/h44z/wg-portal";
