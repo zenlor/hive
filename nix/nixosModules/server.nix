a3101e5d13e01aa9aa6a5f802b6a592bc9bf7bc9
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-12-23 13:25:36 +0100

    add neovim and lnav to servers

--- nix/nixosModules/server.nix
+++ nix/nixosModules/server.nix
@@ -1,6 +1,12 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   services.fail2ban = {
     enable = true;
   };
   services.openssh.settings.LogLevel = "VERBOSE";
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    lnav
+  ];
 }
